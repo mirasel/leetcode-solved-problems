054047be25fc84b2b16e89c00379def0a60bbebb
Mominul Islam Rasal <mominulislamrasel@gmail.com>
2023-02-05 20:26:44 +0600

    Time: 86 ms, Memory: 44.4 MB - LeetHub

---
+++ duplicate-zeros/duplicate-zeros.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+var duplicateZeros = function(arr) {
+    for(let i=0; i<arr.length;i++){
+        if(arr[i]===0&&i!=arr.length-1){
+            for(let j=arr.length-1; j>i+1;j--){
+                arr[j]=arr[j-1]
+            }
+            arr[++i]=0;
+        }
+    }
+};
