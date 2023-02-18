b63b41fba27a5c5cf23d4fe10c5ac7e9e7ab5f71
Mominul Islam Rasal <mominulislamrasel@gmail.com>
2023-02-18 15:49:13 +0600

    Time: 74 ms, Memory: 42.3 MB - LeetHub

--- largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.js
@@ -3,8 +3,14 @@
  * @return {number}
  */
 var dominantIndex = function(nums) {
-    const maxNum = Math.max(...nums);
-    const maxIndex = nums.indexOf(maxNum);
+    let maxNum=-1, maxIndex=0;
+    
+    for(let i=0;i<nums.length; i++){
+        if(nums[i]>maxNum){
+            maxNum = nums[i];
+            maxIndex = i;
+        }
+    }
     
     for(let i=0;i<nums.length; i++){
         if(nums[i]!=maxNum&&2*nums[i]>maxNum){
