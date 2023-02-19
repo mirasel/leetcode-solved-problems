d47c6dacae412c7699f19f4457d9b1018f33d7a2
Mominul Islam Rasal <mominulislamrasel@gmail.com>
2023-02-19 12:29:48 +0600

    Time: 77 ms, Memory: 42.9 MB - LeetHub

--- 0add-binary/0add-binary.js
+++ 0add-binary/0add-binary.js
@@ -36,15 +36,28 @@ var addBinary = function(a, b) {
 //         }
 //     }
 //     return c?'1'+result:result;
+    
+//another way
   let carry = 0;
   let result = '';
   let i = a.length - 1;
   let j = b.length - 1;
 
+//   while (i >= 0 || j >= 0 || carry > 0) {
+//     const sum = parseInt(a[i] || 0) + parseInt(b[j] || 0) + carry;
+//     result = (sum % 2) + result;
+//     carry = Math.floor(sum / 2);
+//     i--;
+//     j--;
+//   }
+
+//   return result;
+    
+//bitwise xor ,and , or operation solution
   while (i >= 0 || j >= 0 || carry > 0) {
-    const sum = parseInt(a[i] || 0) + parseInt(b[j] || 0) + carry;
-    result = (sum % 2) + result;
-    carry = Math.floor(sum / 2);
+    const sum = ((a[i] === '1') ? 1 : 0) ^ ((b[j] === '1') ? 1 : 0) ^ carry;
+    result = sum + result;
+    carry = ((a[i] === '1') & (b[j] === '1')) | ((a[i] === '1') & carry) | ((b[j] === '1') & carry);
     i--;
     j--;
   }
